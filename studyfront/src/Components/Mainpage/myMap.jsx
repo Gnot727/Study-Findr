39a20988fe2971ea4f2986183b862aafeae6844d
Goran <goran.not@ufl.edu>
2025-03-13 15:48:59 -0400

    google maps api integrated

--- studyfront/src/Components/Mainpage/myMap.jsx
+++ studyfront/src/Components/Mainpage/myMap.jsx
@@ -1,27 +1,85 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import React, {useState} from 'react';
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
+//npm install "@react-google-maps/api"
 
-
-//npm install "@vis.gl/react-google-maps"
-//npm install "@googlemaps/markerclusterer"
 
 const MapComponent = () => {
-    //console.log(process.env);
-    const googleAPI = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  const mapStyles = {
+    
+    height: '850px',
+    width: '100%',
+  };
 
-    return(
-    <APIProvider apikey ={googleAPI}>
-        <Map
-        center={{ lat: 29.6516, lng: -82.3248 }}
-        zoom={14}
-        style={{ width: "100%", height: "500px" }}
-        gestureHandling="greedy"
-    >
-        </Map>
-    </APIProvider>
+  const defaultCenter = {
+    lat: 29.6456, // Default latitude
+     lng: -82.3519,  // Default longitude
+   };
+  const libWest = {
+    
+    lat:29.6515513,
+    lng:-82.34281469999999,
+  };
+  const smathersLib = {
+    lat:29.6508246,
+    lng:-82.3417565,
+  }
+  const MarstonLib ={
+    lat: 29.6479572,
+    lng: -82.3439199,
+  };
+  const libraries = [
+    {id: 1, position: MarstonLib, name: "Marston Library"},
+    {id: 2, position: libWest, name: "Library West"},
+    {id: 3, position: smathersLib, name: "Smathers Library"},
+  ]
+  const bounds = {
+    north: 29.675,  
+    south: 29.613,  
+    west: -82.394,  
+    east: -82.315,
+  };
+  // Define custom map styles inside the component
+  const customMapStyle = [
+    {
+      featureType: "poi", // Hide all points of interest (businesses, landmarks, etc.)
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    }
+  ];
+  const ApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  const [selectMarker, setSelectMarker] = useState(null);
 
-);
+  return (
+    <LoadScript googleMapsApiKey={ApiKey}>
+      <GoogleMap 
+        mapContainerStyle={mapStyles} 
+        zoom={15} 
+        center={defaultCenter} 
+        options={{ styles: customMapStyle,
+            restriction: {
+                latLngBounds: bounds,
+                strictBounds: false,
+            }
+        }} // Make sure this is correctly passed
+      >
+        {libraries.map(lib => (
+          <Marker key={lib.id}
+           position={lib.position} 
+           title={lib.name} 
+           onClick={ () => setSelectMarker(lib)}/>
+        ))}
+        {selectMarker && (
+            <InfoWindow position={selectMarker.position} 
+            onCloseClick={ () => setSelectMarker(null)}
+            >
+            <div>
+            <h3>{selectMarker.name}</h3>
+            </div>
+            </InfoWindow>
+        )}
+      </GoogleMap>
+    </LoadScript>
+  );
 };
 
 export default MapComponent;
