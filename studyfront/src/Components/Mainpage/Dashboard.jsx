d06eb001f8b09e63c073e1316b93bfc0a3d2e964
Goran <goran.not@ufl.edu>
2025-04-08 21:20:23 -0400

    Added sidebar and bookmark logo in info window, broken implementation

--- studyfront/src/Components/Mainpage/Dashboard.jsx
+++ studyfront/src/Components/Mainpage/Dashboard.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import MapComponent from "./myMap";
+import Sidebar from "./Sidebar";
 
 const Dashboard = () => {
-    return(
-        <div className="w-screen h-screen overflow-hidden">
-            <MapComponent/>
+    return (
+        <div className="flex w-screen h-screen overflow-hidden">
+            <Sidebar />
+
+            <div className="flex-grow">
+                <MapComponent />
+            </div>
         </div>
     );
 };
